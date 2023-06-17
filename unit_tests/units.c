7da0931e293f0afb9ba13fd41c886fba47b189d2
A-Mahla <A-Mahla>
2023-06-17 16:23:56 +0100

    write testing

--- unit_tests/units.c
+++ unit_tests/units.c
@@ -6,25 +6,69 @@
 #include <stdlib.h>
 
 
+void    ftStrlenTest( void ) {
+
+	printf("\t========== ft_strcpy tests ===========\n\n");
+
+	printf("\t  ft_strlen(\"123456789l\")\t=>\t '%zu' (return value)\n", ft_strlen("123456789l"));
+	printf("\t  strlen(\"123456789l\")\t\t=>\t '%zu' (return value)\n\n", strlen("123456789l"));
+
+	printf("\t  ft_strlen(\"\")\t\t\t=>\t '%zu' (return value)\n", ft_strlen(""));
+	printf("\t  strlen(\"\")\t\t\t=>\t '%zu' (return value)\n\n", strlen(""));
+
+	printf("\t  ft_strlen(\"\\xff\\xff\\xff\\xff\")\t=>\t '%zu' (return value)\n", ft_strlen("\xff\xff\xff\xff"));
+	printf("\t  strlen(\"\\xff\\xff\\xff\\xff\")\t=>\t '%zu' (return value)\n\n", strlen("\xff\xff\xff\xff"));
+
+	printf("\n\n");
+}
+
+
 void    ftStrcpyTest( void ) {
 
 	printf("\t========== ft_strcpy tests ===========\n\n");
 
 	char dst[20] = {0};
 
-	printf("\t  ft_strcpy(dst, \"123456789l\")\t=>\t '%s' (dst value)\n", ft_strcpy(dst, "123456789l"));
-	bzero(dst, 20);
-	printf("\t  strcpy(dst, \"123456789l\")\t=>\t '%s' (dst value)\n\n", strcpy(dst, "123456789l"));
-	bzero(dst, 20);
-
-	printf("\t  ft_strcpy(dst, \"\")\t=>\t '%s' (dst value)\n", ft_strcpy(dst, ""));
-	bzero(dst, 20);
-	printf("\t  strcpy(dst, \"\")\t=>\t '%s' (dst value)\n\n", strcpy(dst, ""));
+	printf(
+        "\t  ft_strcpy(dst, \"123456789l\")\t=>\t '%s' (return value) / '%s' (dst value)\n",
+        ft_strcpy(dst, "123456789l"),
+        dst
+    );
 	bzero(dst, 20);
 
-	printf("\t  ft_strcpy(dst, \"xff\")\t=>\t '%#x' (dst value)\n", ft_strcpy(dst, "\xff")[0]);
+	printf(
+        "\t  strcpy(dst, \"123456789l\")\t=>\t '%s' (return value) / '%s' (dst value)\n\n",
+        strcpy(dst, "123456789l"),
+        dst
+    );
 	bzero(dst, 20);
-	printf("\t  strcpy(dst, \"xff\")\t=>\t '%#x' (dst value)\n\n", strcpy(dst, "\xff")[0]);
+
+	printf(
+        "\t  ft_strcpy(dst, \"\")\t\t=>\t '%s' (return value) / '%s' (dst value)\n",
+        ft_strcpy(dst, ""),
+        dst
+    );
+	bzero(dst, 20);
+
+	printf(
+        "\t  strcpy(dst, \"\")\t\t=>\t '%s' (return value) / '%s' (dst value)\n\n", 
+        ft_strcpy(dst, ""),
+        dst
+    );
+	bzero(dst, 20);
+
+	printf(
+        "\t  ft_strcpy(dst, \"xff\")\t\t=>\t '%#x' (return value) / '%#x' (dst value)\n",
+        ft_strcpy(dst, "\xff")[0]
+        ,dst[0]
+    );
+	bzero(dst, 20);
+
+	printf(
+        "\t  strcpy(dst, \"xff\")\t\t=>\t '%#x' (return value) / '%#x' (dst value)\n\n",
+        strcpy(dst, "\xff")[0],
+        dst[0]
+    );
 	bzero(dst, 20);
 
 	printf("\n\n");
@@ -34,10 +78,56 @@ void    ftStrcpyTest( void ) {
 
 void    ftStrcmpTest( void ) {
 
-  char  *str1 = "asdklfjasdfj////asdf'''asdf3##";
-  char  *str2 = "";
-  printf("%d\n", ft_strcmp(str1, str2));
-  printf("%d\n", strcmp(str1, str2));
+	printf("\t========== ft_strcpy tests ===========\n\n");
+
+	printf("\t  ft_strcmp(\"\", \"\")\t=>\t '%d' (return value)\n", ft_strcmp("", ""));
+	printf("\t  strcmp(\"\", \"\")\t=>\t '%d' (return value)\n\n", strcmp("", ""));
+
+	printf(
+        "\t  ft_strcmp(\"asdklfjasdfj////asdf'''asdf3##\", \"\")\t=>\t '%d' (return value)\n",
+        ft_strcmp("asdklfjasdfj////asdf'''asdf3##", "")
+    );
+	printf(
+        "\t  strcmp(\"asdklfjasdfj////asdf'''asdf3##\", \"\")\t\t=>\t '%d' (return value)\n\n",
+        strcmp("asdklfjasdfj////asdf'''asdf3##", "")
+    );
+
+	printf(
+        "\t  ft_strcmp(\"\", \"asdklfjasdfj////asdf'''asdf3##\")\t=>\t '%d' (return value)\n",
+        ft_strcmp("", "asdklfjasdfj////asdf'''asdf3##")
+    );
+	printf(
+        "\t  strcmp(\"\", \"asdklfjasdfj////asdf'''asdf3##\")\t\t=>\t '%d' (return value)\n\n",
+        strcmp("", "asdklfjasdfj////asdf'''asdf3##")
+    );
+
+	printf(
+        "\t  ft_strcmp(\"Hello World\", \"Hello WOrld\")\t=>\t '%d' (return value)\n",
+        ft_strcmp("Hello World", "Hello WOrld")
+    );
+	printf(
+        "\t  strcmp(\"Hello World\", \"Hello WOrld\")\t\t=>\t '%d' (return value)\n\n",
+        strcmp("Hello World", "Hello WOrld")
+    );
+
+	printf(
+        "\t  ft_strcmp(\"Hello WOrld\", \"Hello World\")\t=>\t '%d' (return value)\n",
+        ft_strcmp("Hello WOrld", "Hello World")
+    );
+	printf(
+        "\t  strcmp(\"Hello WOrld\", \"Hello World\")\t\t=>\t '%d' (return value)\n\n",
+        strcmp("Hello WOrld", "Hello World")
+    );
+
+	printf(
+        "\t  ft_strcmp(\"Hello World\", \"Hello World\")\t=>\t '%d' (return value)\n",
+        ft_strcmp("Hello World", "Hello World")
+    );
+	printf(
+        "\t  strcmp(\"Hello World\", \"Hello World\")\t\t=>\t '%d' (return value)\n\n",
+        strcmp("Hello World", "Hello World")
+    );
+
 
 }
 
@@ -74,9 +164,9 @@ void    ftStrdupTest( void ) {
 
 int     main(void) {
 
-//	ftStrlenTest();
+	ftStrlenTest();
 	ftStrcpyTest();
-//	ftStrcmpTest();
+	ftStrcmpTest();
 //	ftWriteTest();
 //	ftReadTest();
 	ftStrdupTest();
